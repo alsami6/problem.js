d331c279d6e4272a15650f196ca6ecd47bb3e6ad
Al <alsami1209@gmail.com>
2024-06-12 18:17:30 +0600

    array

--- main.js
+++ main.js
@@ -1,2 +1,14 @@
-const numbers = [1, 5, 10, 15, 20]
-console.log(numbers.length);
+const numbers = [88, 99, 77, 421, 54, 63];
+
+
+numbers.push(12);
+
+console.log(numbers);
+
+
+
+const friends = ["Balam", "Kalam", "Abul", "Kashem"]
+friends.push("Khailam")
+console.log(friends);
+friends.pop()
+console.log(friends);
