d9b7c416ea5922d26dfa81505727142b9bb39117
Al <alsami1209@gmail.com>
2024-06-29 17:08:41 +0600

    loop

--- main.js
+++ main.js
@@ -1,64 +1,96 @@
-const numbers = [88, 99, 77, 421, 54, 63];
+// const numbers = [88, 99, 77, 421, 54, 63];
 
 
-numbers.push(12);
+// numbers.push(12);
 
-// console.log(numbers);
-
-
-
-const friends = ["Balam", "Kalam", "Abul", "Kashem"]
-// friends.push("Khailam")
-// console.log(friends);
-// friends.pop()
-// console.log(friends);
-// friends.shift();
-// console.log(friends);
-
-// console.log(friends.includes('Kalam')); 
-
-
-let someNumber = 33;
-
-let stringNumber = String(someNumber);
-// console.log(stringNumber);
-
-
-// ********* Operation ************
-
-let value = 3
-let negValue = -value
-
-// console.log(negValue);
-
-const name = "Sami";
-const repoCount = 30;
-
-// console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
-
-
-// +++++++++++++ string ++++++++++++
-
-
-const gameName = new String('alsami-sm')
-
-
-const newString = gameName.substring(0 ,4) 
-console.log(newString);
+// // console.log(numbers);
 
 
 
-const newStringOne = "    Sami    "
+// const friends = ["Balam", "Kalam", "Abul", "Kashem"]
+// // friends.push("Khailam")
+// // console.log(friends);
+// // friends.pop()
+// // console.log(friends);
+// // friends.shift();
+// // console.log(friends);
 
-// console.log(newStringOne.trim());
+// // console.log(friends.includes('Kalam')); 
 
 
-// ++++++++++++++++++ Array +++++++++++++++++
+// let someNumber = 33;
 
-const marvelHeros = ["Thor", "Ironman", "Spiderman"]
-const dcHeros = ["Superman", "Batman", "Flash"]
+// let stringNumber = String(someNumber);
+// // console.log(stringNumber);
 
 
-marvelHeros.push(dcHeros);
+// // ********* Operation ************
 
-console.log(marvelHeros);
+// let value = 3
+// let negValue = -value
+
+// // console.log(negValue);
+
+// const name = "Sami";
+// const repoCount = 30;
+
+// // console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+
+// // +++++++++++++ string ++++++++++++
+
+
+// const gameName = new String('alsami-sm')
+
+
+// const newString = gameName.substring(0 ,4) 
+// // console.log(newString);
+
+
+
+// const newStringOne = "    Sami    "
+
+// // console.log(newStringOne.trim());
+
+
+// // ++++++++++++++++++ Array +++++++++++++++++
+
+// const marvelHeros = ["Thor", "Ironman", "Spiderman"]
+// const dcHeros = ["Superman", "Batman", "Flash"]
+
+
+// // const allHeros = marvelHeros.concat(dcHeros)
+
+// // console.log(allHeros);
+
+
+// const allNewHeros = [...marvelHeros, ...dcHeros]
+
+// // console.log(allNewHeros)
+
+
+// // console.log(Array.isArray("sami"))
+// // console.log(Array.from("sami"))
+// // console.log(Array.from({name: "sami"})) // interesting case
+
+
+// let score1 = 100;
+// let score2 = 200;
+// let score3 = 300;
+
+// // console.log(Array.of(score1,score2,score3));
+
+// // includes
+// const bondhu = ["sojib", "sami", "nabidul", "raihan"]
+// // console.log(bondhu.includes('sojib'));
+
+// // ++++++++ LOOP +++++++++
+
+
+const numbers = [12, 13, 14, 15, 16, 17]
+
+console.log(numbers);
+
+for (const num of numbers) {
+    console.log(num);
+}
