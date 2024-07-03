ab9481f4010d9cd5f7c5649fb7996e12c1108a82
Al <alsami1209@gmail.com>
2024-07-03 15:13:47 +0600

    _

---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,15 @@
+if(mark >= 80){
+    console.log("You got A+");
+}else if(mark >= 70){
+    console.log("You got A");
+}else if(mark >= 60){
+    console.log("You got A-");
+}else if(mark >= 50){
+    console.log("You got B");
+}else if(mark >= 40){
+    console.log("You got C");
+}else if(mark >= 33){
+    console.log("You got D");
+}else{
+    console.log("You failed");
+}
