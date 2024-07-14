17dbf1f3f7b8b11a7f929f89c1e73225d2898f64
Al <alsami1209@gmail.com>
2024-07-14 14:56:49 +0600

    arr

--- main.js
+++ main.js
@@ -369,22 +369,34 @@
 // console.log(college.unique.result.merit);
 
 
-const mobile = {
-    brand: 'Samsung',
-    price: 25000,
-    color: 'Black',
-    camera: '12mp',
-    isNew: true
+// const mobile = {
+//     brand: 'Samsung',
+//     price: 25000,
+//     color: 'Black',
+//     camera: '12mp',
+//     isNew: true
+// }
+
+// for(const prop in mobile){
+//     // console.log(prop);
+//     // console.log(mobile[prop]);
+// }
+
+// const keys = Object.keys(mobile);
+// console.log(keys);
+
+// for(const key of keys){
+//     console.log(key, mobile[key]);
+// }
+
+
+const friends = ["Elon", "Bill", "Mark", "Waren"];  
+
+for(const friend of friends){
+    // console.log(friend);
 }
 
-for(const prop in mobile){
-    // console.log(prop);
-    // console.log(mobile[prop]);
-}
 
-const keys = Object.keys(mobile);
-console.log(keys);
-
-for(const key of keys){
-    console.log(key, mobile[key]);
+for(let i = 0; i < friends.length; i++){
+    console.log(friends[i]);
 }
